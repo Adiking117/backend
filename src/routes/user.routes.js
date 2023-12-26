0aafa370899a933fc644ea4dba7f99812bd54bed
Adiking117 <www.aditya117@gmail.com>
2023-12-26 20:39:31 +0530

    added meaning comments

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -30,7 +30,7 @@ router.route("/update-account").patch(verifyJWT,updateAccountDetails)
 router.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar)
 router.route("/cover-image").patch(verifyJWT,upload.single("coverImage"),updateUserCoverImage)
 
-router.route("/c/:username").get(verifyJWT,getUserChannelProfile)
+router.route("/c/:username").get(verifyJWT,getUserChannelProfile) // params
 router.route("/history").get(verifyJWT,getWatchHistory)
 
 
